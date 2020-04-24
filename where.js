c228c5c96cf12c34b69b0b10340aa208ad32033e
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-04-24 15:39:57 +0700

    [Fresly] - commit from folder 3 video 11. Where Variables

--- where.js
+++ where.js
@@ -13,13 +13,11 @@ con.connect(function (err) {
   if (err) throw err;
   console.log("Connected to the Database");
 
-  // Where Clause
-  // var sql = "SELECT * FROM customers WHERE id <= 4";
-  // var sql = "SELECT * FROM customers WHERE name = 'Rosmelia Daeli'";
-  // var sql = "SELECT * FROM customers WHERE name LIKE 'J%'";
-  // var sql = "SELECT * FROM customers WHERE name LIKE 'J%' OR id = 1";
-  var sql = "SELECT * FROM customers WHERE name LIKE 'F%' AND id = 1";
-  con.query(sql, function (err, res, fields) {
+  // Where Variables
+  var name_search = 'J%';
+  var id_search = 1;
+  var sql = "SELECT * FROM customers WHERE name LIKE ? OR id = ?";
+  con.query(sql, [name_search, id_search], function (err, res, fields) {
     if (err) throw err;
 
     console.log(res);
