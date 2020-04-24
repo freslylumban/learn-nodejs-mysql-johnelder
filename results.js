001b4f85eaac0a3707edfe66896ad61970f81c37
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-04-24 13:28:20 +0700

    [Fresly] - commit from folder 3 video 4. Result Messages

---
+++ results.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql");
+
+// Create a connection
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "nodedb",
+});
+
+// Connect to MySQL
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected to the Database");
+
+  // Insert into
+  var sql = "INSERT INTO customers (name, email) VALUES ('Steve Rogers', 'steve@gmail.com')";
+  con.query(sql, function (err, res) {
+    if (err) throw err;
+    console.log(res);
+  });
+});
