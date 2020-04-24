0429964f6c7cebf6a09b9e99c0cb72e0662c5dd9
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-04-24 11:51:02 +0700

    [Fresly] - commit from folder 2 video 2. Create a Database

--- hello.js
+++ hello.js
@@ -5,10 +5,16 @@ const con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
+  database: "nodedb",
 });
 
 // Connect to MySQL
 con.connect(function (err) {
   if (err) throw err;
   console.log("Connected to the Database");
+
+  con.query("CREATE DATABASE nodedb", function (err, res) {
+    if (err) throw err;
+    console.log("Database Created!");
+  });
 });
