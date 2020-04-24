ed804c89355b2263a5a2c14e38c827cf784ef9a4
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-04-24 15:35:21 +0700

    [Fresly] - commit from folder 3 video 10. And Or

--- where.js
+++ where.js
@@ -16,7 +16,9 @@ con.connect(function (err) {
   // Where Clause
   // var sql = "SELECT * FROM customers WHERE id <= 4";
   // var sql = "SELECT * FROM customers WHERE name = 'Rosmelia Daeli'";
-  var sql = "SELECT * FROM customers WHERE name LIKE 'J%'";
+  // var sql = "SELECT * FROM customers WHERE name LIKE 'J%'";
+  // var sql = "SELECT * FROM customers WHERE name LIKE 'J%' OR id = 1";
+  var sql = "SELECT * FROM customers WHERE name LIKE 'F%' AND id = 1";
   con.query(sql, function (err, res, fields) {
     if (err) throw err;
 
